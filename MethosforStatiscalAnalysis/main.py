01664d205275da8825761b37aaef51131e6b64c5
Srujan <srujankumar1996@gmail.com>
2021-12-22 15:44:14 +0530

    Added Base Scripts for loading the Data

---
+++ MethosforStatiscalAnalysis/main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import pickle
+import os
+
+path = os.path.dirname(os.path.realpath(__file__))
+# # LoadingDatasets
+# file_name = path+'/LoadingDatasets/load.csv'
+# with open(file_name) as f:
+#     data_csv = pd.read_csv(f)
+#     print(data_csv.head())
+#
+# with open(path+'/LoadingDatasets/load_pickle.pickle',"rb") as fd:
+#     data_picke = pickle.load(fd)
+#     print(data_picke.head())
+
+#PreparingDatasets
+print(path)
+
